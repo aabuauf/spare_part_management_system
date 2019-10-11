e821ca42a933a9b974710ad9fd8a638d18780cb2
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-11 13:57:05 -0400

    fix bom qty

--- app/controllers/spares_controller.rb
+++ app/controllers/spares_controller.rb
@@ -27,13 +27,17 @@ if  !is_valid_user?
 else
   @spare = Spare.new(params_spare)
   @spare.factory = @user.factory
-  params[:spare][:equipment].each do |e|
-    @spare.equipment << Equipment.find_by_tag_no(e)
-  end
-  @spare.boms.each do |s|
-    s.qty_installed = 1
-  end
   @spare.save
+  
+  params[:spare][:equipment].each do |e|
+    sbom = Bom.create(:spare_id => @spare.id, :equipment_id => Equipment.find_by_tag_no(params[:spare][:equipment][0]).id, :qty_installed => params[:spare][:boms][:qty_installed])
+  
+  end
+
+  # @spare.boms.each do |s|
+  #   s.qty_installed = params[:spare][:boms][:qty_installed]
+  # end
+  
   @equipment = @spare.equipment
   redirect_to spare_path(@spare)
 end
